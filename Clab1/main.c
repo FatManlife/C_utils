d569161891c877c6e8e1d3202c7a0172706ef899
FatManlife <fridgeworkout@gmail.com>
2026-09-17 15:19:59 +0300

    last  changes

--- Clab1/main.c
+++ Clab1/main.c
@@ -37,16 +37,16 @@ int main(){
         n++;
 
         if(c + b < 0 && a != 0 ){
-            if (b*b - x == 0) printf("Impartirea la 0 nu exista");
+            if (b*b - x == 0) printf("Arithmetic error");
             else F = (a*cos(x)*cos(x) + c)/(b*b-x); 
         } 
         else if ( c + b > 0 && a == 0) 
         {
-            if (b*b - c*x == 0)printf("Impartirea la 0 nu exista");
+            if (b*b - c*x == 0 || x == 0 || b == 0)printf("Arithmetic error");
             else F = (a*x - log(b*x))/(b*b - c*x);
         }
         else{
-            if(a*a + c + b*x == 0)printf("Impartirea la 0 nu exista");
+            if(a*a + c + b*x == 0 || x == 0)printf("Arithmetic Error");
             else F = (3*sin(x) + 4*log(2*x))/(a*a + c + b*x);
         } 
 
